cb4e86e7d4387c0ee1ab4e5d90247a9fc371bc4f
KK <kamlesh.kumar@advantev.com>
2024-04-18 18:24:29 +0530

    update file and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -10,6 +11,11 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js"], // Add support for TypeScript files
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "src/styles/styles.css", to: "styles" }],
+    }),
+  ],
   module: {
     rules: [
       {
